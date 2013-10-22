0818f5c1dce65569d855ea70633379cc42606ec5
EC2 Default User <ec2-user@ip-10-156-174-44.ap-northeast-1.compute.internal>
2013-10-23 00:13:24 +0900

    update

--- protected/config/main.php
+++ protected/config/main.php
@@ -55,10 +55,10 @@ return array(
          */
 		// uncomment the following to use a MySQL database
 		'db'=>array(
-			'connectionString' => 'mysql:host=127.0.0.1;dbname=news',
+			'connectionString' => 'mysql:host=127.0.0.1;dbname=development',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'root',
 			'charset' => 'utf8',
 		),
 		'errorHandler'=>array(
